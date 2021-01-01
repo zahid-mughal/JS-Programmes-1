ded47341aee188fd75f5193e2f1b94da7c498958
zahidmughal <zahisohna@gmail.com>
2021-01-01 12:52:34 -0800

    built function and onclose process done

--- Height-converter/heightConverter.js
+++ Height-converter/heightConverter.js
@@ -1,8 +1,20 @@
 const ob = require('readline');
 const userInput = ob.createInterface(process.stdin, process.stdout);
-userInput.question("Enter Your Height(in Inches)...", function(I){
-    hightInCm(I);
+userInput.question("Enter Your Height(in Inches)...", function(i){
+    hightInCm(i);
 
 
+userInput.close();
 })
     
+
+userInput.on("close",function(){
+
+console.log("thanks for convert Inches in to Centimetre...")
+
+})
+function hightInCm(inches){
+    cm = 2.54 * inches;
+    console.log(`${inches} Inches Equal to: ${cm}cm`)
+
+}
