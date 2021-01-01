9854933b9aa3068858f6306deb731d29e0ec9ed2
zahidmughal <zahisohna@gmail.com>
2021-01-01 12:04:47 -0800

    readline use, formulae written in process

--- Temp-converter/Temp-converter.js
+++ Temp-converter/Temp-converter.js
@@ -1,9 +1,9 @@
-function farhToCls(c){
+const ob = require('readline');
+let userInput = ob.createInterface(process.stdin, process.stdout);
+userInput.question("Enter Celcius Temperature... ", function(c){
+    // let msg = "";
     const f=9/5*c+32;
-   // console.log("Celsius "+c+" in farenheit is :"+f);
-   return f;
-   console.log(gh);
-}
-let c= 6;
-let x=farhToCls(c);
-console.log(x);
+    console.log(`${c}Celcius in Fahrenheit is: ${f}`);
+
+    process.exit(0);
+})
