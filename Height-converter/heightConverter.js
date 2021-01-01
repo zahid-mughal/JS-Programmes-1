b6c2baad1cd829545ef3f5f614a9e6d25cd8be82
zahidmughal <zahisohna@gmail.com>
2021-01-01 13:31:53 -0800

    make question in to question and display two inputs

--- Height-converter/heightConverter.js
+++ Height-converter/heightConverter.js
@@ -1,20 +1,23 @@
 const ob = require('readline');
 const userInput = ob.createInterface(process.stdin, process.stdout);
+userInput.question("Enter Your Name...", function(n){
+   
+
 userInput.question("Enter Your Height(in Inches)...", function(i){
+    // console.log(`Hi ${n} </br>  ${hightInCm(i)}`);
+    console.log(`Hi ${n}`);
     hightInCm(i);
-
-
+   
 userInput.close();
 })
-    
-
-userInput.on("close",function(){
-
-console.log("thanks for convert Inches in to Centimetre...")
-
 })
-function hightInCm(inches){
-    cm = 2.54 * inches;
-    console.log(`${inches} Inches Equal to: ${cm}cm`)
+userInput.on("close",function(){
+console.log("thanks for convert Inches in to Centimetre...")
+})
 
+
+
+function hightInCm(inches){
+    cm = parseFloat( 2.54 * inches);
+    console.log(`${inches} Inches Equal to: ${cm}cm`)
 }
