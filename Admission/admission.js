7d719e4eb9c1cf46d2a22dcebd5ceba8fe9e3870
zahidmughal <zahisohna@gmail.com>
2021-01-01 14:02:09 -0800

    make 4 queestion for 4 inputs

---
+++ Admission/admission.js
@@ -0,0 +1,15 @@
+const ob = require('readline');
+let userInput = ob.createInterface(process.stdin,process.stdout);
+userInput.question("Enter Your Name",function(n){
+
+userInput.question("Enter Your 1st Subject Marks: ",function(m1){
+userInput.question("Enter Your 2nd Subject Marks: ",function(m2){
+userInput.question("Enter Your 3rd Subject Marks: ",function(m3){
+ 
+    userInput.close();
+
+
+});
+});
+});
+});
