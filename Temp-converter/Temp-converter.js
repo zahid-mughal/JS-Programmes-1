17194fb61b4d2dc168688477f7ce909662a8d226
zahidmughal <zahisohna@gmail.com>
2021-01-01 12:08:03 -0800

    on close add

--- Temp-converter/Temp-converter.js
+++ Temp-converter/Temp-converter.js
@@ -3,7 +3,13 @@ let userInput = ob.createInterface(process.stdin, process.stdout);
 userInput.question("Enter Celcius Temperature... ", function(c){
     // let msg = "";
     const f=9/5*c+32;
-    console.log(`${c}Celcius in Fahrenheit is: ${f}`);
+    console.log(`${c} Celcius in Fahrenheit is: ${f}`);
 
-    process.exit(0);
+    userInput.close();
+})
+userInput.on("close",function(){
+
+console.log("thanks for your time.");
+
+
 })
