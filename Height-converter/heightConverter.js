3b4a15104e1e7ae209b707dfec0a5430e7af3d4c
zahidmughal <zahisohna@gmail.com>
2021-01-01 12:39:28 -0800

    call function

--- Height-converter/heightConverter.js
+++ Height-converter/heightConverter.js
@@ -1,6 +1,8 @@
 const ob = require('readline');
 const userInput = ob.createInterface(process.stdin, process.stdout);
 userInput.question("Enter Your Height(in Inches)...", function(I){
+    hightInCm(I);
+
 
 })
     
