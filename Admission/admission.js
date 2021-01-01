f4db782106677d287216c3791d49218f226415d6
zahidmughal <zahisohna@gmail.com>
2021-01-01 14:58:10 -0800

    in float, no function

--- Admission/admission.js
+++ Admission/admission.js
@@ -8,10 +8,16 @@ userInput.question("Enter Your 3rd Subject Marks: ",function(m3){
  let onReject = "Your Marks Below 80, Rejected!";
  let onSucces = "Your Marks Above 80, Granted for Admission";
 
- 
-    
- 
- 
+let sum = parseFloat(m1) +parseFloat(m2) +parseFloat(m3);
+ let average =parseFloat(sum/3);
+
+ if(average > 80){
+    console.log(`Mr/Ms.${n} ${onSucces}`);
+ }else{
+    console.log(`Mr/Ms.${n} ${onReject}`);
+
+ };
+
  
  userInput.close();
 });
