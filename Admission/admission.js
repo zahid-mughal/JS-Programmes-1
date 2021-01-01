32d4514883169278733e06f801f3434cb038613e
zahidmughal <zahisohna@gmail.com>
2021-01-01 14:06:16 -0800

    two message on marks

--- Admission/admission.js
+++ Admission/admission.js
@@ -1,14 +1,19 @@
 const ob = require('readline');
 let userInput = ob.createInterface(process.stdin,process.stdout);
-userInput.question("Enter Your Name",function(n){
+userInput.question("Enter Your Name: ",function(n){
 
 userInput.question("Enter Your 1st Subject Marks: ",function(m1){
 userInput.question("Enter Your 2nd Subject Marks: ",function(m2){
 userInput.question("Enter Your 3rd Subject Marks: ",function(m3){
+ let onReject = "Your Marks Below 80, Rejected!";
+ let onSucces = "Your Marks Above 80, Granted for Admission";
+
  
-    userInput.close();
-
-
+    
+ 
+ 
+ 
+ userInput.close();
 });
 });
 });
